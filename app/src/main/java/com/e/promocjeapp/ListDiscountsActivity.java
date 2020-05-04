e716dfc7fa27573fc4b2f8fc39434c4652fa6b27
dgoleniewski <goleniewski.damian1@gmail.com>
2020-05-04 10:40:13 +0200

    work

--- app/src/main/java/com/e/promocjeapp/ListDiscountsActivity.java
+++ app/src/main/java/com/e/promocjeapp/ListDiscountsActivity.java
@@ -22,21 +22,42 @@ public class ListDiscountsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_discounts);
-        //mDatabase = FirebaseDatabase.getInstance().getReference();
+        mDatabase = FirebaseDatabase.getInstance().getReference();
 
         Discount discount = new Discount();
-        discount.setBasicPrice(249);
-        discount.setDiscountPrice(159);
-        discount.setTitle("Słuchawki Pioneer SE-S3BT-H różne kolory");
-        discount.setLink("https://www.pepper.pl/visit/thread/259334");
-        discount.setDescription("Słuchawki Pioneer SE-S3BT-H\n" +
-                "Najniższa cena na Allegro 219 zł\n" +
-                "Dostępne 4 kolory czarne, niebieskie, szare i różowe\n" +
-                "Analogicznie ta sama oferta na Oleole");
+        discount.setBasicPrice(3297);
+        discount.setDiscountPrice(4710);
+        discount.setTitle("Zegarek Longines Hydro Conquest automatic");
+        discount.setLink("https://www.marko.pl/pl/p/Zegarek-meski-Longines-L37424966-Hydro-Conquest/48998");
+        discount.setDescription("Newsletter -10% nie działa na zegarki\n" +
+                "\n" +
+                "Cena finalna: 3297 zł\n" +
+                "Model L37424966\n" +
+                "Automat\n" +
+                "\n" +
+                "Szukam zegarka i natknąłem się na taką cene chyba najniższa jaką spotkałem.\n" +
+                "\n" +
+                "W ofercie mają dużo przecen mi osobiście ten model się podoba w tym rozmiarze.\n" +
+                "\n" +
+                "Wysyłka z polski i gwarancja 24 miesiące\n" +
+                "\n" +
+                "Pierwsza wrzutka...\n" +
+                "\n" +
+                "\n" +
+                "Opis:\n" +
+                "\n" +
+                "\n" +
+                "Longines L37424966 Hydro Conquest to elegancki zegarek męski. Wyposażony jest w mechanizm automatyczny z 64-godzinną rezerwą naciągu, który do zasilania wykorzystuje naturalny ruch ręki, na której noszony jest zegarek. Szkiełko szafirowe zapewnia bardzo wysoką odporność na zarysowania oraz przejrzystość. Zarówno koperta, jak i bransoleta, wykonane ze stali szlachetnej w kolorze srebrnym podkreślają elegancki charakter zegarka. Wodoszczelność na poziomie 300 m (30 ATM, 30 Bar) zapewnia możliwość nurkowania z zegarkiem bez obaw o jego uszkodzenie.\n" +
+                "\n" +
+                "Sprzedawany przez firmę MARKO zegarek jest fabrycznie nowym, oryginalnym produktem firmy Longines, pochodzącym z legalnej i oficjalnej dystrybucji. Zapakowany w firmowe opakowanie, nadaje się idealnie na prezent.\n" +
+                "\n" +
+                "Istnieje możliwość obejrzenia go na żywo w sklepach MARKO oraz sklepach partnerskich (informacja o punktach znajduje się w zakładce O nas).\n" +
+                "\n" +
+                "Model zegarka objęty jest 2 letnią gwarancją producenta oraz Pakietem Korzyści Marko.");
         discounts = new LinkedList<>();
         discounts.add(discount);
 
-        //mDatabase.setValue(discounts);
+        mDatabase.setValue(discounts);
         recyclerView = findViewById(R.id.recyclerView);
         adapter = new MyRecyclerViewAdapter(this, discounts);
         recyclerView.setAdapter(adapter);
